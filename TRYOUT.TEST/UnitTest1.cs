12dd498a626ee105e8f36fbdf559401f1ed78981
eeddd <jnath.orgel@gmail.com>
2023-03-09 14:44:42 +0800

    Unit Test for Base64Service

--- TRYOUT.TEST/UnitTest1.cs
+++ TRYOUT.TEST/UnitTest1.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.SignalR.Client;
 using Moq;
 using System.Threading.Tasks;
 using TRYOUT.Hubs;
+using TRYOUT.Services;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TRYOUT.TEST
 {
@@ -12,7 +14,17 @@ namespace TRYOUT.TEST
         [Fact]
         public async Task Test1()
         {
-           
+            // Arrange
+            IBase64Service service = new Base64Service();
+            var inputText = "Hello";
+            var expectedValue = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(inputText));
+
+            // Act
+            var result = service.ConvertToBase64(inputText);
+
+
+            // Assert
+            Assert.Equal(expectedValue, result);
 
         }
     }
